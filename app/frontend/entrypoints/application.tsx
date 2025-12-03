c3ce08bb76d663d0479c364234e64d6ae64086bc
Eric Laquer <LaquerEric@gmail.com>
2025-12-03 14:30:05 -0500

    works with styling

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -4,22 +4,12 @@ import './application.css'
 
 const pages = import.meta.glob('../pages/**/*.tsx', { eager: true })
 
-console.log('All available pages:', Object.keys(pages))
-
 createInertiaApp({
     resolve: (name) => {
-        console.log('Resolving:', name)
-        const pagePath = `../pages/${name}.tsx`
-        console.log('Looking for:', pagePath)
-        const page = pages[pagePath]
-        console.log('Found page module:', page)
-        if (!page) {
-            throw new Error(`Page not found: ${name}. Available: ${Object.keys(pages).join(', ')}`)
-        }
+        const page = pages[`../pages/${name}.tsx`]
         return page.default
     },
     setup({ el, App, props }) {
-        console.log('Setting up with el:', el)
         createRoot(el).render(<App {...props} />)
     },
 })
